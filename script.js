770495f40ae2e39cb82050f7be191209e1b032a4
Abhishek Sateesh Gaonkar <85872303+ABdevs29@users.noreply.github.com>
2021-06-23 19:30:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("I'm JavaScript!");
+alert('Hello') // Semicolon gets automatically added by JS
+alert(`Wor
+ ld`) // Here, the words "Wor" and "ld" are showed on separate lines in broswer due to the presence of enter key in between the string "World";
+alert(3 +
+1
++ 2); // These multiple line of code are working since the task is for adding and the brackets are defined properly. So, the JS Engine will consider those multiple lines of code as the same and execute the addition operation.
