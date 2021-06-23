611ec43dc650428775c5c2a7ea6c760e47610515
Abhishek Sateesh Gaonkar <85872303+ABdevs29@users.noreply.github.com>
2021-06-23 19:52:15 +0530

    Add files via upload

---
+++ Assignment-6/Find the Culprit-1/Question-5/script.js
@@ -0,0 +1,5 @@
+let fname=10.5; 
+fname = "Guvi";
+lname = "geek";
+let name = fname+ " " + lname; // Add space between Guvi and geek
+alert( `hello, ${name}` ); //Use Backticks instead of single or double quotes
