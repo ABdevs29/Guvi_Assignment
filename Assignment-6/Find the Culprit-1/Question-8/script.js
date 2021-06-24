7fde85de2929b32b8abe75dc5a9c317a430d8b2d
Abhishek Sateesh Gaonkar <85872303+ABdevs29@users.noreply.github.com>
2021-06-24 06:28:01 +0530

    Delete Assignment-6/Find the Culprit-1/Question-8 directory

--- Assignment-6/Find the Culprit-1/Question-8/script.js
+++
@@ -1,9 +0,0 @@
-let a = prompt("Enter a number?"); // We can get Success in console if we don't provide any value in the prompt
-//Don't modify any code below this
-if (a) {
- console.log( 'OMG it works for any number inc 0' );
-}
-else
-{
- console.log( "Success" );
-}
