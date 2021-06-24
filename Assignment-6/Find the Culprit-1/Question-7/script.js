3ef1711675cf1d7afaceee36aac4d2953a7bc0e6
Abhishek Sateesh Gaonkar <85872303+ABdevs29@users.noreply.github.com>
2021-06-24 06:27:54 +0530

    Delete Assignment-6/Find the Culprit-1/Question-7 directory

--- Assignment-6/Find the Culprit-1/Question-7/script.js
+++
@@ -1,9 +0,0 @@
-var a = parseInt("2") > parseInt("12"); //Code was blasting because we had not typecasted the string to number. So, the comparison was not working properly
-//Don't touch below this
-if (a) {
-  console.log("Code is Blasted")
-}
-else
-{
-  console.log("Diffused") 
-}
