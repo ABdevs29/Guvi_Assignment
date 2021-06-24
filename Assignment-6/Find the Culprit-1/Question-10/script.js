d6f41a4e754a8b5f1e16c74bc7daa50d49b972d4
Abhishek Sateesh Gaonkar <85872303+ABdevs29@users.noreply.github.com>
2021-06-24 06:27:01 +0530

    Delete Assignment-6/Find the Culprit-1/Question-10 directory

--- Assignment-6/Find the Culprit-1/Question-10/script.js
+++
@@ -1,6 +0,0 @@
-let login = 'Employee';
-let message = (login == 'Employee') ? 'Welcome': //If Employee logs in, display Welcome.
-  (login == 'Director') ? 'Greetings' :
-  (login == '') ? 'No login' :
-  '';
-console.log(message);
