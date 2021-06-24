0ac478213ca2da5cde93ed098071a95c2edf554e
Abhishek Sateesh Gaonkar <85872303+ABdevs29@users.noreply.github.com>
2021-06-24 06:27:38 +0530

    Delete Assignment-6/Find the Culprit-1/Question-5 directory

--- Assignment-6/Find the Culprit-1/Question-5/script.js
+++
@@ -1,5 +0,0 @@
-let fname=10.5; 
-fname = "Guvi";
-lname = "geek";
-let name = fname+ " " + lname; // Add space between Guvi and geek
-alert( `hello, ${name}` ); //Use Backticks instead of single or double quotes
